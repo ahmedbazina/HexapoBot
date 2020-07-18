76098e8a693e8e46dc1b2f73ea67f9946787022f
Ahmed Bazina <ahmed_bazina2011@yahoo.com>
2020-07-18 18:03:49 +0100

    Cleaned up and created seperate functions

--- Krowdster_bot.py
+++ Krowdster_bot.py
@@ -1,27 +1,18 @@
+import gspread 
 from selenium import webdriver
 from time import sleep
-import gspread 
 from oauth2client.service_account import ServiceAccountCredentials
 from pprint import pprint
 from creds import username, password
 
-scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
-
-credentials = ServiceAccountCredentials.from_json_keyfile_name('bot-creds.json', scope)
-
-client = gspread.authorize(credentials)
-
-sheet = client.open('HexaPo Cost Sheet').get_worksheet(5)
-
-data = sheet.get_all_records()
-data2= sheet.row_values(2)
-data3= sheet.col_values(2)
-
 
 class KrowdsterBot():
+    
+
     def __init__(self):
         self.driver = webdriver.Chrome()
 
+    
     def login(self):
         self.driver.get('https://app.krowdster.co/login')
 
@@ -39,6 +30,7 @@ class KrowdsterBot():
         popup_1 = self.driver.find_element_by_xpath('/html/body/div[9]/div/div[3]/div[2]/button[1]')
         popup_1.click()
 
+    
     def Select(self):
         backer_btn = self.driver.find_element_by_xpath('//*[@id="wrapper"]/article/div/div/div[1]/div/a')
         backer_btn.click()
@@ -53,13 +45,30 @@ class KrowdsterBot():
 
         Find_btn = self.driver.find_element_by_xpath('//*[@id="wrapper"]/article/div[2]/div[2]/div/div/div/div/div/div[4]/form/div/div/div[5]/button[1]/span')
         Find_btn.click()
-
-
     
-bot = KrowdsterBot()
-bot.login()
-bot.Select()
 
-pprint(data)
-pprint(data2)
-pprint(data3)
+def sheets():
+    scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('bot-creds.json', scope)
+
+    client = gspread.authorize(credentials)
+
+    sheet = client.open('HexaPo Cost Sheet').get_worksheet(5)
+
+    data = sheet.get_all_records()
+    data2= sheet.row_values(2)
+    data3= sheet.col_values(2)
+
+    pprint(data)
+    pprint(data2)
+    pprint(data3)
+
+
+def Run(): 
+    bot = KrowdsterBot()
+    bot.login()
+    bot.Select()
+    sheets()
+
+Run()
