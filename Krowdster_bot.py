c56804bf12052cf63530c6dd9c1a68b4d6dc1518
Ahmed Bazina <ahmed_bazina2011@yahoo.com>
2020-07-21 13:42:58 +0100

    Added comments and improved the sleep time

--- Krowdster_bot.py
+++ Krowdster_bot.py
@@ -1,21 +1,34 @@
-import gspread 
+# https://www.youtube.com/watch?v=lvFAuUcowT4
+#Imports the web driver and sleep from selenium & time 
 from selenium import webdriver
 from time import sleep
+
+# Import Google Sheets & Oauth2client to work with Gsheets api
+import gspread 
 from oauth2client.service_account import ServiceAccountCredentials
+
+# Pretty print to visualise data nicely 
 from pprint import pprint
+
+# Create a creds.py and add username and password to extract login details from their
 from creds import username, password
 
 class KrowdsterBot():
-    
-
+     
     def __init__(self):
+        # Start web driver chrome
+
         self.driver = webdriver.Chrome()
-
-
+ 
     def login(self):
-                
-        self.driver.get('https://app.krowdster.co/login')
+        # Login function saving xpath elements in variables with actions that send login details from creds.py and click on buttons:
+        # Open the site 
+        # Enter email, password & click login button
+        # Sleep for 5 seconds until popup appear then close it
+        # inititate/Run SelectFilters           
 
+        self.driver.get('https://app.krowdster.co/login')
+ 
         email_in = self.driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/form/div[1]/input')
         email_in.send_keys(username)
 
@@ -34,6 +47,8 @@ class KrowdsterBot():
 
 
     def SelectFilters(self):
+        # Select filters function saving xpath elements in variables with actions that click on buttons & sleep for the page to load 
+
         backer_btn = self.driver.find_element_by_xpath('//*[@id="wrapper"]/article/div/div/div[1]/div/a')
         backer_btn.click()
 
@@ -43,17 +58,18 @@ class KrowdsterBot():
         Category_btn = self.driver.find_element_by_xpath('//*[@id="wrapper"]/article/div[2]/div[2]/div/div/div/div/div/div[4]/form/div/div/div[2]/select/option[15]')
         Category_btn.click()
 
-        sleep(2)
+        sleep(5)
 
         Find_btn = self.driver.find_element_by_xpath('//*[@id="wrapper"]/article/div[2]/div[2]/div/div/div/div/div/div[4]/form/div/div/div[5]/button[1]/span')
         Find_btn.click()
 
-        sleep(2)
+        sleep(5)
 
         bot.AddToSheet()
-
-
+ 
     def FindSingleUser(self):
+        # Find single user function for debugging
+
         Pic = self.driver.find_element_by_xpath('//*[@id="wrapper"]/article/div[2]/div[2]/div/div/div/div/div/div[4]/div/div[1]/div[1]/div/div[1]/div[5]/a/img')
         Profilepic = Pic.get_attribute('src')
         print(Profilepic)
@@ -86,42 +102,70 @@ class KrowdsterBot():
         Backed = Backer.text
         print(Backed)
 
-
     def AddToSheet(self):
-        scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+        # Function that initiates connection with Gsheets api and dynamically add the data from the site
+        # Add scope for api
+        # Credentials to access api
+        # inititate client connection to Gsheets api
+        # Client access sheet by name and get worksheet by index
+
+        scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
         credentials = ServiceAccountCredentials.from_json_keyfile_name('bot-creds.json', scope)
         client = gspread.authorize(credentials)
         sheet = client.open('HexaPo Cost Sheet').get_worksheet(5)
 
-        for i in range(1,25):
-            Pic = self.driver.find_element_by_xpath("//*[@id='wrapper']/article/div[2]/div[2]/div/div/div/div/div/div[4]/div/div[1]/div[{}]/div/div[1]/div[5]/a/img".format(i)).get_attribute('src')
+        # For loop to get all backers on the page starting at 24 stopping at 0 in reverse order (downwards)
+        # Insert row starting at index
 
-            Name = self.driver.find_element_by_xpath("//*[@id='wrapper']/article/div[2]/div[2]/div/div/div/div/div/div[4]/div/div[1]/div[{}]/div/div[2]/div/h4/a".format(i)).text
+        for i in range(24, 0, -1):
+            Pic = self.driver.find_element_by_xpath('//*[@id="wrapper"]/article/div[2]/div[2]/div/div/div/div/div/div[4]/div/div[1]/div[{}]/div/div[1]/div[5]/a/img'.format(i)).get_attribute('src')
+            Profile = '=IMAGE("{}")'.format(Pic)
 
-            Loc = self.driver.find_element_by_xpath("//*[@id='wrapper']/article/div[2]/div[2]/div/div/div/div/div/div[4]/div/div[1]/div[{}]/div/div[2]/div/div".format(i)).text
+            Name = self.driver.find_element_by_xpath('//*[@id="wrapper"]/article/div[2]/div[2]/div/div/div/div/div/div[4]/div/div[1]/div[{}]/div/div[2]/div/h4/a'.format(i)).text
+
+            Loc = self.driver.find_element_by_xpath('//*[@id="wrapper"]/article/div[2]/div[2]/div/div/div/div/div/div[4]/div/div[1]/div[{}]/div/div[2]/div/div'.format(i)).text
 
             Categ = self.driver.find_element_by_xpath('//*[@id="wrapper"]/article/div[2]/div[2]/div/div/div/div/div/div[4]/form/div/div/div[2]/select/option[15]').text
 
-            KS = self.driver.find_element_by_xpath("//*[@id='wrapper']/article/div[2]/div[2]/div/div/div/div/div/div[4]/div/div[1]/div[{}]/div/div[3]/div/div[1]/a[1]".format(i)).get_attribute('href')
+            KS = self.driver.find_element_by_xpath('//*[@id="wrapper"]/article/div[2]/div[2]/div/div/div/div/div/div[4]/div/div[1]/div[{}]/div/div[3]/div/div[1]/a[1]'.format(i)).get_attribute('href')
 
-            FB = self.driver.find_element_by_xpath("//*[@id='wrapper']/article/div[2]/div[2]/div/div/div/div/div/div[4]/div/div[1]/div[{}]/div/div[3]/div/div[1]/a[2]".format(i)).get_attribute('href')
+            FB = self.driver.find_element_by_xpath('//*[@id="wrapper"]/article/div[2]/div[2]/div/div/div/div/div/div[4]/div/div[1]/div[{}]/div/div[3]/div/div[1]/a[2]'.format(i)).get_attribute('href')
 
-            TW = self.driver.find_element_by_xpath("//*[@id='wrapper']/article/div[2]/div[2]/div/div/div/div/div/div[4]/div/div[1]/div[{}]/div/div[3]/div/div[1]/a[3]".format(i)).get_attribute('href')
+            TW = self.driver.find_element_by_xpath('//*[@id="wrapper"]/article/div[2]/div[2]/div/div/div/div/div/div[4]/div/div[1]/div[{}]/div/div[3]/div/div[1]/a[3]'.format(i)).get_attribute('href')
 
-            Backer = self.driver.find_element_by_xpath("//*[@id='wrapper']/article/div[2]/div[2]/div/div/div/div/div/div[4]/div/div[1]/div[{}]/div/div[1]/div[1]".format(i)).text.strip('Backed')
+            Backer = self.driver.find_element_by_xpath('//*[@id="wrapper"]/article/div[2]/div[2]/div/div/div/div/div/div[4]/div/div[1]/div[{}]/div/div[1]/div[1]'.format(i)).text.strip('Backed')
 
-            row = [Pic, Name, Loc, Categ, KS, FB, TW, Backer]
+            row = [Profile, Name, Loc, Categ, KS, FB, TW, Backer]
             index = 2
             sheet.insert_row(row, index)
 
-        data = sheet.get_all_records()
-        data2 = sheet.row_values(2)
-        data3 = sheet.col_values(2)
+        # Variables to read all records, single row or column within the worksheet stated above 
+        # Print what was read
 
-        pprint(data)
-        pprint(data2)
-        pprint(data3)
+        # allRecords = sheet.get_all_records()
+        # pprint(allRecords)
+
+        # Row = sheet.row_values(2)
+        # pprint(Row)
+
+        # Column = sheet.col_values(2)
+        # pprint(Column)
 
 
+    # Auto function to run the whole script 
+
+    # def auto_run(self):
+    #     while True:
+    #         sleep(0.5)
+    #         try:
+    #             self.like()
+    #         except Exception:
+    #             try:
+    #                 self.close_popup()
+    #             except Exception:
+    #                 self.close_match()
+
+
+# Create an instanance of the bot and Run login function
 bot = KrowdsterBot()
 bot.login()
--- krowdster.py
+++ krowdster.py
@@ -4,6 +4,7 @@ from time import sleep
 from oauth2client.service_account import ServiceAccountCredentials
 from pprint import pprint
 from creds import username, password
+import re
 
 
 driver = webdriver.Chrome("C:\webDrivers\chromedriver.exe")
@@ -27,7 +28,7 @@ Platform_btn.click()
 Category_btn = driver.find_element_by_xpath('//*[@id="wrapper"]/article/div[2]/div[2]/div/div/div/div/div/div[4]/form/div/div/div[2]/select/option[15]')
 Category_btn.click()
 
-sleep(2)
+sleep(5)
 
 Find_btn = driver.find_element_by_xpath('//*[@id="wrapper"]/article/div[2]/div[2]/div/div/div/div/div/div[4]/form/div/div/div[5]/button[1]/span')
 Find_btn.click()
@@ -38,12 +39,12 @@ def data():
 	scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 	credentials = ServiceAccountCredentials.from_json_keyfile_name('bot-creds.json', scope)
 	client = gspread.authorize(credentials)
-	sheet = client.open('HexaPo Cost Sheet').get_worksheet(6)
+	sheet = client.open('HexaPo Cost Sheet').get_worksheet(5)
 	
 	# For loop to get all backers on the page starting at 24 stopping at 0 in reverse order (downwards)
 	for i in range(24, 0, -1):
 		Pic = driver.find_element_by_xpath("//*[@id='wrapper']/article/div[2]/div[2]/div/div/div/div/div/div[4]/div/div[1]/div[{}]/div/div[1]/div[5]/a/img".format(i)).get_attribute('src')
-		image = "=IMAGE({})".format(Pic)
+		image = '=IMAGE("{}")'.format(Pic)
 
 		Name = driver.find_element_by_xpath("//*[@id='wrapper']/article/div[2]/div[2]/div/div/div/div/div/div[4]/div/div[1]/div[{}]/div/div[2]/div/h4/a".format(i)).text
 
@@ -65,6 +66,14 @@ def data():
 		index = 2
 		sheet.insert_row(row, index)
 
-
-
+    def auto_swipe(self):
+        while True:
+            sleep(0.5)
+            try:
+                self.like()
+            except Exception:
+                try:
+                    self.close_popup()
+                except Exception:
+                    self.close_match()
 data()	
